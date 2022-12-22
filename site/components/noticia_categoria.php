f8625eca6730c978c61dccce9394ac86f1d8cc1c
livia128924 <livia128924@gmail.com>
2022-12-21 21:18:44 -0300

    atualizando

--- site/components/noticia_categoria.php
+++ site/components/noticia_categoria.php
@@ -43,7 +43,12 @@ border-radius: 0;
               </div> -->
               <h3 class="post-title" style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;"><?=$d->titulo?></h3>
               <p style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;"><?=strip_tags(str_replace('<',' <',str_replace('>','> ',$d->materia)))?></p>
-              <a href="noticia.php?cod=<?=$d->codigo?>" class="readmore stretched-link"><span>Leia Mais</span><i class="bi bi-arrow-right"></i></a>
+              
+              <a href="noticia.php?cod=<?=$d->codigo?>" class="">
+                <button type="button" class="btn btn-outline-success">Leia Mais<i class="bi bi-arrow-right"></i></button>
+              </a>
+
+            
             </div>
           </div>
 
