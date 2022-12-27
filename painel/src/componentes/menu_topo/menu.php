a0bfa947ebf278b08376f0866f6bb4ae324dcead
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-27 17:53:41 -0400

    Atualizações

--- painel/src/componentes/menu_topo/menu.php
+++ painel/src/componentes/menu_topo/menu.php
@@ -11,13 +11,12 @@
 <nav class="navbar navbar-expand bg-light">
   <div class="container-fluid">
     <div data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-
-    <div  class="d-none d-sm-block">
-    <img src="img/logocred2.png" style="height:43px; margin-right:20px;" >
-</div>
-
-
-      <i class="fa-solid fa-bars"></i>
+      <div class="d-flex justify-content-start">
+        <div  class="d-none d-sm-block">
+          <img src="img/logocred2.png" style="height:43px; margin-right:20px;" >
+        </div>
+        <i class="fa-solid fa-bars"></i>
+      </div>
     </div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
