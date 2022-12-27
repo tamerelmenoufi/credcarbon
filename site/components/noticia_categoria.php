0c83b324b2ff2543e3d0b5a9f3b440f2b5bd6144
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-27 15:14:35 -0400

    Atualizações

--- site/components/noticia_categoria.php
+++ site/components/noticia_categoria.php
@@ -19,7 +19,7 @@
       <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-          <h2 style="">Todos os serviços</h2>
+          <h2 style="">Serviços</h2>
 
         </div>
 
