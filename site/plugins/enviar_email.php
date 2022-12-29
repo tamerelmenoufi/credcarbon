5192e73aec13e554459dde394097ce500f670c7d
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-29 15:11:01 -0400

    Atualizações

--- site/plugins/enviar_email.php
+++ site/plugins/enviar_email.php
@@ -75,11 +75,7 @@
 
         $result = file_get_contents("http://137.184.15.211/mailgun/send.php", null, $context);
         #############################################################################
-        echo $result;
         $result = json_decode($result);
-        // print_r($result);
-
-        // echo "<h1>".$result->0; //->['status']."</h1>";
 
         if($result->status == 'success'){
             echo "Mensagem enviada com sucesso!";
