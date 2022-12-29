26317852acb0de361fee3e70c447ca48ce9c527b
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-29 15:21:10 -0400

    Atualizações

--- site/components/rodape.php
+++ site/components/rodape.php
@@ -1,4 +1,4 @@
-  
+
   <style>
 
 .footer .footer-legal {
@@ -6,11 +6,11 @@
   background: #057a34!important;
 }
   </style>
-  
+
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
 
-   
+
     <div class="footer-legal text-center">
       <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
 
@@ -23,16 +23,36 @@
             <!-- You can delete the links only if you purchased the pro version. -->
             <!-- Licensing information: https://bootstrapmade.com/license/ -->
             <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
-          
+
           </div>
         </div>
 
         <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
-          <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-          <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
-          <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        <?php
+
+          $query = "select * from configuracoes where codigo = '1'";
+          $result = mysqli_query($con, $query);
+          $d = mysqli_fetch_object($result);
+
+          $midias = json_decode($d->midias_sociais);
+
+          $midias_sociais = [
+            'facebook' => 'https://www.facebook.com/',
+            'twitter' => 'https://twitter.com/',
+            'instagram' => 'https://www.instagram.com/',
+            'youtube' => 'https://www.youtube.com/',
+            'linkedin' => 'https://www.linkedin.com/',
+            'whatsapp' => 'https://api.whatsapp.com/send?phone='
+          ];
+
+          foreach($midias_sociais as $ind => $url){
+            if($midias->$ind){
+        ?>
+        <a href="<?=$url.$midias->$ind?>" target="_black" class="<?=$ind?>"><i class="bi bi-<?=$ind?>"></i></a>
+        <?php
+            }
+          }
+        ?>
         </div>
 
       </div>
