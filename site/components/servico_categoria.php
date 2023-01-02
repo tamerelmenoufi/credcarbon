f1c85a4c4565cf0e3dc2243b7e7796b69228f4b2
livia128924 <livia128924@gmail.com>
2023-01-02 18:55:39 -0300

    atualizando

--- site/components/servico_categoria.php
+++ site/components/servico_categoria.php
@@ -19,7 +19,7 @@
       <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-          <h2 style="">Serviços</h2>
+          <h2 style="">Todos os Serviços</h2>
 
         </div>
 
