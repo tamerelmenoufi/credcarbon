037c62bb76fe96cb637465d2f78bf4a0cfde3916
livia128924 <livia128924@gmail.com>
2023-01-02 18:46:23 -0300

    atualizando

--- site/components/noticias.php
+++ site/components/noticias.php
@@ -1,54 +1,50 @@
     <!-- ======= Recent Blog Posts Section ======= -->
-<section id="recent-blog-posts" class="recent-blog-posts">
 
-<style>
+    <style>
+  .recent-blog-posts .post-box:hover .post-title {
+    color: #057a34;}
 
-  .botaoazul{
-    background: var(--color-primary);
-  border: 0;
-  padding: 10px 35px;
-  color: #fff;
-  transition: 0.4s;
-  border-radius: 0;
-  }
+    .botaoverde{
+      padding: 15px;
+    padding-left: 35px;
+    padding-right: 35px;
+    border: 1px solid #198754;
+    background: #198754;
+    color: #fff;
+    border-radius: 25px 2px 25px;
+    font-size: 17px;
+    }
 
-</style>
+  </style>
+    <section id="servicos"  style="padding:0px" class="recent-blog-posts">
 
-    <!-- ======= Breadcrumbs ======= -->
-    <div style="font-weight:450;background:#057a34;color:#fff" class="breadcrumbs">
-      <div class="container">
+      <div class="container" data-aos="fade-up">
 
-        <div class="d-flex justify-content-between align-items-center">
-          <h2 style="">Serviços</h2>
-
+        <div class="section-header">
+          <h2>Noticias</h2>
+          <p>CredCaron Manaus  está em demanda em todo o mundo e vai transformar o universo de investimentos. Os ganhos futuros serão reflexo de uma estrutura consolidada e soluções para as maiores urgências do século.</p>
         </div>
 
-      </div>
-    </div><!-- End Breadcrumbs -->
- <div class="container aos-init aos-animate">
         <div class="row">
 
           <?php
-          $query = "select * from noticias where situacao = '1' order by codigo desc";
+          $query = "select * from noticias where situacao = '1'";
           $result = mysqli_query($con, $query);
           while($d = mysqli_fetch_object($result)){
           ?>
 
-          <div style="margin-top:15px" class="col-lg-3" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
             <div class="post-box">
-              <div class="post-img"><img src="<?=$localPainel?>src/volume/noticias/<?=$d->imagem?>" class="img-fluid" alt=""></div>
+              <div style="margin-top:10px" class="post-img"><img src="<?=$localPainel?>src/volume/noticias/<?=$d->imagem?>" class="img-fluid" alt=""></div>
               <!-- <div class="meta">
                 <span class="post-date">Tue, December 12</span>
                 <span class="post-author"> / Julia Parker</span>
               </div> -->
               <h3 class="post-title" style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;"><?=$d->titulo?></h3>
               <p style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;"><?=strip_tags(str_replace('<',' <',str_replace('>','> ',$d->materia)))?></p>
-
-              <a href="servico.php?cod=<?=$d->codigo?>" class="mt-3 mb-3">
+              <a href="noticia.php?cod=<?=$d->codigo?>" class="mt-3 mb-3">
                 <button type="button" class="btn btn-outline-success">Leia Mais<i class="bi bi-arrow-right"></i></button>
               </a>
-
-
             </div>
           </div>
 
@@ -82,31 +78,16 @@
             </div>
           </div> -->
 
+
+          <center style="margin-top:20px">
+          <a href="servico_categoria.php">
+         <button type="button" class="botaoverde">
+          Outros serviços
+        </button></a>
+      </center>
+
+
         </div>
-
-<!-- <center style="margin-top:20px">
-  <nav aria-label="Page navigation example">
-    <ul class="pagination justify-content-center">
-      <li class="page-item disabled">
-        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
-      </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
-      <li class="page-item">
-        <a class="page-link" href="#">Proximo</a>
-      </li>
-    </ul>
-  </nav>
-</center> -->
-
-        <div class="col-lg-12" style="padding:10px">
-
-    <a style="color:#fff" href="javascript:history.back()">
-    <button style="margin-top:10px"  type="button"  class="float-end  botaoazul">
-                Voltar</button></a>
- </div>
-</div>
 
       </div>
 
