d492b96d15e9d32127c13b3fb94a3061f2d01eff
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-13 22:10:58 -0400

    Atualizações

--- painel/lib/includes.php
+++ painel/lib/includes.php
@@ -11,5 +11,5 @@
 
     $md5 = md5(date("YmdHis"));
 
-    $localPainel = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/credcarbon/painel/";
-    $localSite = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/credcarbon/site/";
+    $localPainel = $_SERVER["REQUEST_SCHEME"]."://206.81.10.165:8180/credcarbon/painel/";
+    $localSite = $_SERVER["REQUEST_SCHEME"]."://206.81.10.165:8180/credcarbon/site/";
