e0dad7dd0d9129528a3277c45503f9cf0270cfae
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-13 23:19:30 -0400

    Atualizações

--- mailgun/index.php
+++ mailgun/index.php
@@ -2,6 +2,7 @@
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
     $_POST = json_decode(file_get_contents('php://input'), true);
 
-    $post = addslashes(json_encode($_POST));
+    // $post = addslashes(json_encode($_POST));
+    $post = var_dump($_POST);
 
-    file_put_contents('anexos/'.date("YmdHis").".txt", var_dump($post));
+    file_put_contents('anexos/'.date("YmdHis").".txt", $post);
