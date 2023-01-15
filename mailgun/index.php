eafa49c5928ba806383125cca4c2efd6ed375bd7
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-15 16:15:10 -0400

    Atualizações

--- mailgun/index.php
+++ mailgun/index.php
@@ -1,9 +1,18 @@
 <?php
+
+    date_default_timezone_set("America/Manaus");
+    include("/credcarbonoinc/connect.php");
+    $con = AppConnect('emails');
+
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
     $_POST = json_decode(file_get_contents('php://input'), true);
 
-    // $post = addslashes(json_encode($_POST));
+    $mailgun = addslashes(json_encode($_POST));
     $post = print_r($_POST, true)."\n<hr>\n".print_r($_GET, true);
+
+    $result = mysqli_query($con, "insert into mailgun set email = '{$mailgun}', data = NOW()");
+    $registro = mysqli_insert_id($result);
 
     $attachments = json_decode($_POST['attachments']);
     $dados = false;
@@ -16,7 +25,9 @@
 
         $arquivo = file_get_contents(str_replace("://","://api:key-f084b8985ece92fcbbc2948cfe7f855f@",$arq->url));
 
-        file_put_contents("anexos/{$arq->name}", $arquivo);
+        if(!is_dir("anexos/{$registro}")) mkdir("anexos/{$registro}");
+
+        file_put_contents("anexos/{$registro}/{$arq->name}", $arquivo);
 
     }
 
