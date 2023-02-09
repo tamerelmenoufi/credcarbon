567f1a3590e67b2dd1e65db5b3784ce02a4075f2
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-09 11:28:54 -0400

    Atualizações

--- painel/src/paginas_topicos/topicos_form.php
+++ painel/src/paginas_topicos/topicos_form.php
@@ -86,28 +86,7 @@
 <script>
 
     ClassicEditor
-    .create( document.querySelector( '#descricao_topico' ), {
-        toolbar: [
-            // 'exportPDF','exportWord', '|',
-            // 'findAndReplace', 'selectAll', '|',
-            // 'heading', '|',
-            'bold', 'italic',
-            // 'strikethrough', 'underline', 'code', 'subscript', 'superscript', 'removeFormat', '|',
-            'bulletedList', 'numberedList',
-            // 'todoList', '|', //*****
-            // 'outdent', 'indent', '|',
-            // 'undo', 'redo',
-            // '-',
-            // 'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'highlight', '|', //*****
-            'alignment', '|', //*****
-            // 'link', 'insertImage',
-            // 'blockQuote', 'insertTable', 'mediaEmbed', 'codeBlock', //*****
-            //  'htmlEmbed', '|',
-            // 'specialCharacters', 'horizontalLine', 'pageBreak', '|',
-            // 'textPartLanguage', '|',
-            // 'sourceEditing'
-         ]
-    })
+    .create( document.querySelector( '#descricao_topico'))
     .then( editor => {
         console.log( editor );
     } )
