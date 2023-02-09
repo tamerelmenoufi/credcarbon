e6c92fe2f4202483417b5ec85eb060bb0a3341bb
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-09 10:21:33 -0400

    Atualizações

--- site/components/sobre.php
+++ site/components/sobre.php
@@ -10,24 +10,9 @@
 <style>
   .sombra{
     /* offset-x | offset-y | color */
-box-shadow: 60px -16px teal;
-
-/* offset-x | offset-y | blur-radius | color */
-box-shadow: 10px 5px 5px black;
-
-/* offset-x | offset-y | blur-radius | spread-radius | color */
-box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
-
-/* inset | offset-x | offset-y | color */
-box-shadow: inset 5em 1em gold;
-
-/* Any number of shadows, separated by commas */
-box-shadow: 3px 3px red, -1em 0 0.4em olive;
-
-/* Global keywords */
-box-shadow: inherit;
-box-shadow: initial;
-box-shadow: unset;
+    -webkit-box-shadow: -35px 32px 14px rgba(29, 140, 231, 0.45);
+		-moz-box-shadow:    -35px 32px 14px rgba(29, 140, 231, 0.45);
+		box-shadow:         -35px 32px 14px rgba(29, 140, 231, 0.45);
 
   }
 </style>
