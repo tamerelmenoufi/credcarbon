96dbb783b20dd272a4a64abdc9a2600d1f48846c
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-01 16:43:25 +0200

    Atualizações

--- painel/src/componentes/menu_topo/menu.php
+++ painel/src/componentes/menu_topo/menu.php
@@ -56,47 +56,21 @@
             </li>
         </ul>
 
-        <div class="menuAplicativos">
-        <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-                <a
-                    class="nav-link d-flex align-items-center justify-content-center"
-                    href="#" id="navbarScrollingDropdown2"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    ria-expanded="false"
-                    style="
-                            background-color:#eee;
-                            border-radius:100%;
-                            width:40px;
-                            height:40px;
-                            font-weight:bold;
-                          "
-                >
-                  <i class="fa-brands fa-buromobelexperte" style="font-size:25px;"></i>
-                </a>
-                <ul class="dropdown-menu  dropdown-menu-end" aria-labelledby="navbarScrollingDropdown2">
-                    <li class="MenuLogin">
-                      <div class="row">
-                        <div class="col-4">1</div>
-                        <div class="col-4">1</div>
-                        <div class="col-4">1</div>
-                        <div class="col-4">1</div>
-                        <div class="col-4">1</div>
-                        <div class="col-4">1</div>
-                        <div class="col-4">1</div>
-                        <div class="col-4">1</div>
-                        <div class="col-4">1</div>
-                        <div class="col-4">1</div>
-                        <div class="col-4">1</div>
-                      </div>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        </div>
+        <div class="menuAplicativos"></div>
     </div>
   </div>
 </nav>
 
 
+<script>
+  $(function(){
+
+    $.ajax({
+      url:"src/componentes/menu_topo/aplicativos.php",
+      success:function(dados){
+        $(".menuAplicativos").html(dados);
+      }
+    })
+
+  })
+</script>
