b15ebfef5687bb894bcfe55cefc9b4554286b195
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-01 17:13:51 +0200

    Atualizações

--- painel/src/componentes/menu_topo/aplicativos.php
+++ painel/src/componentes/menu_topo/aplicativos.php
@@ -42,21 +42,21 @@
                     <div class="col-4">
                         <div class="card w-100 d-flex align-items-center justify-content-center appIcons" >
                             <h3><i class="fa-solid fa-envelope-open-text"></i></h3>
-                            <span class="fs-6">E-mail</span>
+                            <span>E-mail</span>
                         </div>
                     </div>
 
                     <div class="col-4">
                         <div class="card w-100 d-flex align-items-center justify-content-center appIcons" >
                             <h3><i class="fa-solid fa-calendar-days"></i></h3>
-                            <span class="fs-6">Agenda</span>
+                            <span>Agenda</span>
                         </div>
                     </div>
 
                     <div class="col-4">
                         <div class="card w-100 d-flex align-items-center justify-content-center appIcons" >
                             <h3><i class="fa-solid fa-house"></i></h3>
-                            <span class="fs-6">Site</span>
+                            <span>Site</span>
                         </div>
                     </div>
 
