b4cbaa03d5def04a3fac3cce660ea746e621fcc6
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-02 15:30:27 +0200

    Atualizações

--- painel/email/home/caixa_entrada.php
+++ painel/email/home/caixa_entrada.php
@@ -36,28 +36,50 @@
 </style>
 <div class="pagina">
     <div class="row relativo">
-        <div class="col-4 relativo">
-            <div class="listaEntrada">
-                <?php
-                for($i=0;$i<100;$i++){
-                ?>
-                Lista de entrada<br>
-                <?php
-                }
-                ?>
+
+        <!-- Exibe apenas no PC -->
+        <div class="d-none d-sm-block relativo">
+            <div class="col-4 relativo">
+                <div class="listaEntrada">
+                    <?php
+                    for($i=0;$i<100;$i++){
+                    ?>
+                    Lista de entrada<br>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="exibeEmail">
+                    <?php
+                    for($i=0;$i<100;$i++){
+                    ?>
+                    Exibição dos e-mails<br>
+                    <?php
+                    }
+                    ?>
+                </div>
             </div>
         </div>
-        <div class="col-8 d-none d-sm-block relativo">
-            <div class="exibeEmail">
-                <?php
-                for($i=0;$i<100;$i++){
-                ?>
-                Exibição dos e-mails<br>
-                <?php
-                }
-                ?>
+
+
+        <!-- Exibe apenas no celular -->
+        <div class="d-none d-sm-block relativo">
+            <div class="col relativo">
+                <div class="listaEntrada">
+                    <?php
+                    for($i=0;$i<100;$i++){
+                    ?>
+                    Lista de entrada<br>
+                    <?php
+                    }
+                    ?>
+                </div>
             </div>
         </div>
+
+
     </div>
 </div>
 
