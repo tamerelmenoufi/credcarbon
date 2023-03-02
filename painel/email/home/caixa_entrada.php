9184fad4bfcc83a11b5b1276a560da9a46f7dc7d
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-02 14:36:33 +0200

    Atualizações

--- painel/email/home/caixa_entrada.php
+++ painel/email/home/caixa_entrada.php
@@ -1,8 +1,12 @@
 <?php
         include("{$_SERVER['DOCUMENT_ROOT']}/credcarbon/painel/lib/includes.php");
 ?>
-
-<div class="container">
+<style>
+    .pagina{
+        background-color:#fff;
+    }
+</style>
+<div class="container pagina">
     <div class="row">
         <div class="col-4">
             Lista de entrada
