b3654016f7c60345035bef6adf66fe66206e49ab
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-05 16:32:01 +0200

    Atualizações

--- painel/email/entrada/home.php
+++ painel/email/entrada/home.php
@@ -89,7 +89,7 @@
 
                         <div>
                             <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input ms-2" id="exampleCheck1">
+                                <input type="checkbox" class="form-check-input ms-1" id="exampleCheck1">
                                 <!-- <label class="form-check-label" for="exampleCheck1">Marcar Todos</label> -->
                             </div>
                         </div>
