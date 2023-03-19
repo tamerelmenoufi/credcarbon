8c08367ac91c959d0d2c041a108fbce2975c380c
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-20 00:09:27 +0200

    Atualizações

--- chat/index.php
+++ chat/index.php
@@ -21,7 +21,7 @@
 
 <div class="ms_corpo"></div>
 
-
+<?php include("{$_SERVER['DOCUMENT_ROOT']}/credcarbon/painel/lib/footer.php"); ?>
 
 <script src="<?= "js/app.js?" . date("YmdHis"); ?>"></script>
 <script src="<?= "js/wow.js"; ?>"></script>
@@ -53,6 +53,5 @@
 
 </script>
 <form></form>
-<?php include("{$_SERVER['DOCUMENT_ROOT']}/credcarbon/painel/lib/footer.php"); ?>
 </body>
 </html>
