9d847fc32b80618d095a33d61091af84f557f575
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-31 09:51:19 +0200

    Atualizações

--- site/assets/lib/log_acessos.php
+++ site/assets/lib/log_acessos.php
@@ -1,7 +1,7 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/credcarbon/site/assets/lib/includes.php");
 
-    $dados = json_encode($_SERVER);
+    $dados = str_replace("\n","\\n",json_encode($_SERVER));
 
     echo $query = "insert into log_acessos set data = NOW(), dados = '{$dados}'";
     mysqli_query($con, $query);
