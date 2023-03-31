21090cb106ab173437776906d46a93ac43e9a609
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-31 14:11:41 +0200

    Atualizações

--- painel/chats/entrada/home.php
+++ painel/chats/entrada/home.php
@@ -156,7 +156,14 @@
                         }
                         ?>
                     </div>
-                    <div class="exibeEmailRodape"></div>
+                    <div class="exibeEmailRodape">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <i class="fa-regular fa-face-smile"></i>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <i class="fa-solid fa-microphone"></i>
+                            <i class="fa-regular fa-paper-plane"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
