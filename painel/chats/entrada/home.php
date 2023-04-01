7828d32dbef28291d7661a4021ff8ef9d2dc80db
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-01 13:01:11 +0200

    Atualizações

--- painel/chats/entrada/home.php
+++ painel/chats/entrada/home.php
@@ -152,10 +152,10 @@
                         <?php
                         for($i=0;$i<100;$i++){
                         ?>
-                        <div class="d-flex m-1 p-2" style="background-color:#ffffff; border:0; border-radius:20px; width:auto;">
+                        <span class="d-flex m-1 p-2" style="background-color:#ffffff; border:0; border-radius:20px; width:auto;">
                             <div>Texto da mensagem enviada</div>
                             <div class="text-end" style="color:#b6a29a; font-size:10px;">12:17</div>
-                        </div>
+                        </span>
                         <?php
                         }
                         ?>
