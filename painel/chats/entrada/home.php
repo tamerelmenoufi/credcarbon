e24affc94c99fd10c038876557d1692430d0d675
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-04 03:03:38 +0200

    Atualizações

--- painel/chats/entrada/home.php
+++ painel/chats/entrada/home.php
@@ -151,9 +151,16 @@
                     <div class="exibeEmail">
                         <?php
                         for($i=0;$i<100;$i++){
+                            if($i%2 == 0){
+                                $reverse = '-reverse';
+                                $cor = '#dcf8c6';
+                            }else{
+                                $reverse = false;
+                                $cor = '#ffffff';
+                            }
                         ?>
-                        <div class="d-flex flex-row-reverse">
-                        <div class="d-inline-flex flex-column m-1 p-2" style="max-width:60%; background-color:#ffffff; border:0; border-radius:20px;">
+                        <div class="d-flex flex-row<?=$reverse?>">
+                        <div class="d-inline-flex flex-column m-1 p-2" style="max-width:60%; background-color:<?=$cor?>; border:0; border-radius:20px;">
                             <div class="text-start" style="border:solid 0px red;">Texto da mensagem enviada Texto da mensagem enviada Texto da mensagem enviada Texto da mensagem enviada Texto da mensagem enviada </div>
                             <div class="text-end" style="color:#b6a29a; font-size:10px; border:solid 0px black;">12:17</div>
                         </div>
