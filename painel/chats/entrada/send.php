1d53105e9dcaf5432132aa791c1ce2b542df8661
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-05 03:28:51 +0200

    Atualizações

--- painel/chats/entrada/send.php
+++ painel/chats/entrada/send.php
@@ -2,7 +2,7 @@
         include("{$_SERVER['DOCUMENT_ROOT']}/credcarbon/painel/lib/includes.php");
 
         $dados = [
-            'mensagem' => $_POST['msg'],
+            'mensagem' => 'Resposta de: '.$_POST['msg'],
         ];
 
         $dados = json_encode($dados);
