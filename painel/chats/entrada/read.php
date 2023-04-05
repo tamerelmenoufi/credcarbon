d060f4eca9df82b02bc6b4046c5b567bc6b86103
tamerelmenoufi <tamer@mohatron.com.br>
2023-04-05 03:36:43 +0200

    Atualizações

--- painel/chats/entrada/read.php
+++ painel/chats/entrada/read.php
@@ -1,7 +1,7 @@
 <?php
         include("{$_SERVER['DOCUMENT_ROOT']}/credcarbon/painel/lib/includes.php");
 
-        $dados = file_get_contents('caixa/msg.json', $dados);
+        $dados = file_get_contents('caixa/msg.json');
 
         echo $dados = json_decode($dados);
 
